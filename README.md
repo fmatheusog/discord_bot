0cbbee1dcbf92268729ab6ff14178fbbf2202334
Felipe Mori <felipe.mori@onfriday.com.br>
2023-04-04 16:54:23 -0300

    feat: implements random kick slash command

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Slash Commands to-do list:
 
 - [ ] /joined: mostra quando o membro entrou no servidor 
-- [ ] /online: mostra quantos membros do servidor estão online
+- [x] /online: mostra quantos membros do servidor estão online
 - [x] /confession: envia uma confissão para um canal definido (pode ser anônima ou não)
-- [ ] /kickroulette: expulsa um membro aleatório do servidor
+- [x] /kickroulette: expulsa um membro aleatório do servidor
--- src/commands/RandomKick.ts
+++ src/commands/RandomKick.ts
@@ -4,8 +4,20 @@ import { Command } from '../interfaces/Command';
 export const RandomKick: Command = {
   data: new SlashCommandBuilder()
     .setName('randomkick')
-    .setDescription('Bane um membro aleatório do servidor')
+    .setDescription('Expulsa um membro aleatório do servidor')
     ,
   run: async (interaction) => {
+    const user = interaction.guild?.members.cache.get(interaction.user.id);
+    const isAdmin = user?.roles.cache.some((r) => r.name == 'Donos do Bar');
+
+    if(isAdmin) {
+      const members = await interaction.guild?.members.fetch();
+      const randomMember = members?.random();
+
+      await interaction.reply({ content: `Usuário expulso: ${randomMember?.displayName}`, ephemeral: true });
+      // randomMember?.kick('Expulso pelo comando random kick');
+    } else {
+      await interaction.reply('Safadinho, você não tem permissão para utilizar este comando!');
+    }
   }
 }
