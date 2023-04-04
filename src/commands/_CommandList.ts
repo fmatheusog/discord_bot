98687ff50bd3e2f616767633d2b7de55136a4aee
Felipe Mori <felipe.mori@onfriday.com.br>
2023-04-04 14:53:14 -0300

    refact: changes AddMessage filename to Confession

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,5 +1,5 @@
 import { Command } from '../interfaces/Command';
-import { Confession } from './AddMessage';
+import { Confession } from './Confession';
 import { RandomKick } from './RandomKick';
 
 export const CommandList: Command[] = [ Confession, RandomKick ];
