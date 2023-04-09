7ed9056a6d9770de3e5a802b33ccdf7b722fccdf
Felipe Mori <felipe.mori@onfriday.com.br>
2023-04-08 23:50:41 -0300

    feat: implements who is online slash command

---
+++ src/commands/WhoIsOnline.ts
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Command } from '../interfaces/Command';
+
+export const WhoIsOnline: Command = {
+  data: new SlashCommandBuilder()
+    .setName('whoisonline')
+    .setDescription('Mostra todos os membros online do servidor'),
+  run: async (interaction) => {
+    const members = await interaction.guild?.members.fetch();
+    const onlineMembers = members
+      ?.filter((m) => m.presence?.status !== undefined && m.presence?.status !== 'offline')
+      .map((m) => m.displayName);
+
+    const response = onlineMembers?.join(`\n`);
+
+    await interaction.reply(response as string);
+  }
+}
